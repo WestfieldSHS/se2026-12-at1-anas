dc823613df37cef317a7dbaa7b7591eeb24db587
Anas Almahdawi <anas.almahdawi@education.nsw.gov.au>
2026-02-15 17:29:47 +1100

    i basically finsihed the layout of page 1 so i filled in the layout for all the pages

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True  # ← ADD THIS LINE
 
 
 @app.route("/")
@@ -11,6 +12,26 @@ def index():
 @app.route("/page1")
 def page1():
     return render_template("page1.html")
+
+
+@app.route("/page2")
+def page2():
+    return render_template("page2.html")
+
+
+@app.route("/page3")
+def page3():
+    return render_template("page3.html")
+
+
+@app.route("/page4")
+def page4():
+    return render_template("page4.html")
+
+
+@app.route("/page5")
+def page5():
+    return render_template("page5.html")
 
 
 if __name__ == "__main__":
